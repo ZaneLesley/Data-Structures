bcf1ea2fd42d260f334c5ca14c348e04602f96cd
Zane Lesley <zanelesley@ou.edu>
2024-05-28 21:50:18 -0500

    Updated containsDuplicate.cpp to go fast mode
    https://neetcode.io/problems/duplicate-integer

--- Arrays_and_Hashing/containsDuplicate.cpp
+++ Arrays_and_Hashing/containsDuplicate.cpp
@@ -1,27 +1,24 @@
 #include <vector>
 #include <cstdio>
+#include <unordered_map>
 
 using namespace std;
 
 /* 
-
-Basically a quick recap back into c++ and vectors
-
-Code is pretty simple, iterate through all of the numbers checking for duplicate on the vector.
-Can start j at i + 1 becasue we have already checked the number previous to j, so no need to check again.
-
+Went back and reimplemented with umap. 
+We keep the key and value of the hash, incrementing it by 1 for each count, and if there is any count over 1 we know there is a duplicate.
  */
 
 bool hasDuplicate(vector<int>& nums){
-    for (int i = 0; i < nums.size(); i++){
-        for (int j = i + 1; j < nums.size(); j++){
-            if(nums[i] == nums[j])
-                return true;
+    unordered_map<int, int> umap;
+    for(int i = 0; i < nums.size(); i++){
+        umap[nums[i]]++;                        //Initializes to 0 if not made, and moves to 1.
+        if(umap[nums[i]] > 1){                  //That is why here we check bigger than 1 instead of bigger than 0.
+            return true;
         }
     }
 
     return false;
-
 };
 
 int main(){
