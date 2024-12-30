129424af7c604ee2f535d8ebc5d806d85a5149af
ZaneLesley <Zanelesley@outlook.com>
2024-12-30 13:55:54 -0600

    feat: added search.py for binary_search

---
+++ Binary_Search/search.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = int((left + right) / 2)
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+
+
+if '__main__' == __name__:
+    solution = Solution()
+    print(solution.search([-1, 0, 2, 4, 6, 8], 4))
